6b168a48ccf1f5ecbd413ec2c64b26a063e808f6
Harsh Dhorajia <hdhorajia@q3tech.com>
2023-06-29 17:18:16 +0530

    initial setup

---
+++ server/models/contacts.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const Contacts = sequelize.define('contacts', {
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    linkedId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    linkPreference: {
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      type: 'TIMESTAMP',
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+      allowNull: false,
+    },
+    updatedAt: {
+      type: 'TIMESTAMP',
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+      allowNull: false,
+    },
+    deletedAt: {
+      type: 'TIMESTAMP',
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+      allowNull: false,
+    },
+  });
+  return Contacts;
+};
